4925abd59b9143626adf17488cccfe2d4183d153
Tom Hicks <tom.hicks@me.com>
2014-01-16 00:26:40 +0000

    setup and first tests

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['test/**/*.js', 'src/**/*.js'],
+        tasks: ['test']
+      },
+    },
+  });
+
+  grunt.registerTask('test', 'mochaTest:test');
+
+};
